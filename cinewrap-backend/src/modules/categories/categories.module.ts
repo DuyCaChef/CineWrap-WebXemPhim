b32597caea797e327975def797ececf2ff96ca03
DuyCaChef <duylun72@gmail.com>
2026-05-27 00:33:07 +0700

    (api categories): Khai báo thêm RolesGuard để ở module để tránh lỗi Dependency

--- cinewrap-backend/src/modules/categories/categories.module.ts
+++ cinewrap-backend/src/modules/categories/categories.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { RolesGuard } from '../auth/guards/roles.guard'; // Import RolesGuard để đăng ký trong module
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [CategoriesService, RolesGuard], // Đăng ký RolesGuard để có thể sử dụng trong Controller, tránh lỗi Dependency Injection khi dùng @UseGuards(RolesGuard)
 })
 export class CategoriesModule {}
