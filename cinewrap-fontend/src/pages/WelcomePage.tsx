a26bf93e0514e5be02bf3525e2b14993d2719671
DuyCaChef <duylun72@gmail.com>
2026-06-12 15:16:17 +0700

    style(WelcomePage)

--- cinewrap-fontend/src/pages/WelcomePage.tsx
+++ cinewrap-fontend/src/pages/WelcomePage.tsx
@@ -1483,7 +1483,6 @@ const WelcomePage: React.FC = () => {
                       stroke="currentColor"
                       strokeWidth="2"
                     >
-                      cd
                       <polyline
                         points="6 9 12 15 18 9"
                         strokeLinecap="round"
