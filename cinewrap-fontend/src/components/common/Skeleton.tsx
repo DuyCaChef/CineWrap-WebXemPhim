8e877c1b54d1833807b4008fc22bb4caee932999
DuyCaChef <duylun72@gmail.com>
2026-08-03 13:17:12 +0700

    feat(components/common/Skeletion.tsx): Xây dựng một Base Component để dùng chung cho các hiệu ứng Shimmer

---
+++ cinewrap-fontend/src/components/common/Skeleton.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface SkeletonProps {
+  className?: string;
+}
+export const Skeleton: React.FC<SkeletonProps> = ({ className = "" }) => {
+  return (
+    <div className={`animate-pulse rounded-md bg-slate-800/80 ${className}`} />
+  );
+};
